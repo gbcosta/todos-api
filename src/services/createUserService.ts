f37cb41ae4b480ecf0a4b02a1d14edc9c813e854
gbcosta <gbcostasant@gmail.com>
2022-01-26 18:42:19 -0300

    update user servicet

--- src/services/createUserService.ts
+++ src/services/createUserService.ts
@@ -3,7 +3,7 @@ import { PrismaClient } from "@prisma/client";
 export class CreateUserService {
   async execute({ name, email }) {
     const prisma = new PrismaClient();
-    console.log(name, email);
+
     if (!name && !email) {
       throw new Error("Name or email are required");
     }
